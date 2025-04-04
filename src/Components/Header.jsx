0d7a20094794160437766867eeeaa6c337d952b2
BIG_DEV360 <shittuabdquadri0@gmail.com>
2025-04-04 05:22:14 -0700

    ww

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -18,7 +18,7 @@ const Header = () => {
 
     return (
         <div className="sticky top-0 w-full bg-white shadow-md z-50" id="home">
-            <div className="flex justify-between text-black">
+            <div className="flex justify-between text-black p-4 md:p-2">
                 <img src={Aphamd} alt="Aphamed Logo" className="size-[50px]" />
 
                 {/* Desktop Navigation */}
